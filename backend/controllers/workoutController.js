6f4a4a2a42b8dbf7bbfc68debeac9d2bb69b4dcb
Ahmed-Asban <ahmdmrsban99@gmail.com>
2025-02-12 19:22:31 +0300

    Modified files

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,14 +1,38 @@
 const Workout = require('../models/workoutModel')
 
+const mongoose = require('mongoose')
+
 
 // get all workout
+const getWorkouts = async (req, res) => {
+
+    const workouts = await Workout.find({}).sort({ createdAt: -1 })
+
+    res.status(200).json(workouts)
+
+}
 
 
 // get a single workout
+const getWorkout = async (req, res) => {
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findById(id)
+
+    if (!workout) {
+        return res.status(400).json({ error: 'No such workout' })
+    }
+
+    res.status(200).json(workout)
+}
 
 
 // create a new workout
-const  createWorkout = async (req, res) => {
+const createWorkout = async (req, res) => {
     const { title, reps, load } = req.body
 
     try {
@@ -28,6 +52,8 @@ const  createWorkout = async (req, res) => {
 
 
 // exporting functions
-module.exports= {
-    createWorkout
+module.exports = {
+    getWorkouts,
+    getWorkout,
+    createWorkout,
 }
