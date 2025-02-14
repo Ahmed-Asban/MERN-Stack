980a16bedfb8d2e21e8522e3fc7bdffdf4ba143f
Ahmed-Asban <ahmdmrsban99@gmail.com>
2025-02-14 17:17:52 +0300

    Modifying and adding new file Home.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// pages & components
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={ <Home /> }>
+
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
